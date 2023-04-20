722a334d00a96decb1647df85911833977fdb747
YooJin <yoojin4719@gmail.com>
2023-04-20 18:09:56 +0900

    Add files via upload

---
+++ portfolio/portfolio/js/moviedesign.js
@@ -0,0 +1,9 @@
+const moviedesign_container = document.querySelector(".moviedesign_container");
+// const items = document.querySelector("");
+// const item = document.querySelectorAll(".moviedesign_item");
+
+window.onload = () => {
+  setTimeout(function () {
+    moviedesign_container.style.display = "block";
+  }, 200);
+};
--- portfolio/portfolio/js/script.js
+++ portfolio/portfolio/js/script.js
@@ -1,1 +1,7 @@
+// const name = "장유진";
+// const search_input = document.querySelector(".search_input");
 
+// if ((search_input.value = name)) {
+// } else {
+//   alert("다시");
+// }
