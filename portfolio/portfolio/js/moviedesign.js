eb672a2eb74a279a521494844a08211c6e1d899d
YooJin <yoojin4719@gmail.com>
2023-04-24 18:19:32 +0900

    Add files via upload

--- portfolio/portfolio/js/moviedesign.js
+++ portfolio/portfolio/js/moviedesign.js
@@ -1,6 +1,6 @@
 const moviedesign_container = document.querySelector(".project_container");
-const item = document.querySelectorAll(".moviedesign_item");
 const detail = document.querySelectorAll(".detail");
+const item = document.querySelectorAll(".moviedesign_item");
 
 window.onload = () => {
   setTimeout(function () {
@@ -8,8 +8,49 @@ window.onload = () => {
   }, 200);
 };
 
-for (i = 0; i < item.length; i++) {
-  item[i].addEventListener("click", function () {
-    this.lastElementChild.classList.toggle("open");
-  });
+// for (i = 0; i < item.length; i++) {
+//   item[i].addEventListener("click", function () {
+//     this.lastElementChild.classList.toggle("open");
+//   });
+// }
+
+// const content = document.querySelectorAll(".content");
+
+// content.forEach(function (e) {
+//   e.addEventListener("click", toggleAccordion);
+// });
+
+// function toggleAccordion(e) {
+//   let detailbox = e.currentTarget.nextElementSibling.classList;
+//   // let plus = e.currentTarget.classList;
+
+//   if (detailbox.contains("open")) {
+//     detailbox.remove("open");
+//     // plus.remove("open");
+//   } else {
+//     detail.forEach(function (e) {
+//       e.classList.remove("open");
+//     });
+//     detailbox.add("open");
+//     // plus.add("open");
+//   }
+// }
+
+const content = document.querySelectorAll(".content");
+
+item.forEach(function (e) {
+  e.addEventListener("click", toggleAccordion);
+});
+
+function toggleAccordion(e) {
+  let detailbox = e.currentTarget.classList;
+
+  if (detailbox.contains("open")) {
+    detailbox.remove("open");
+  } else {
+    item.forEach(function (e) {
+      e.classList.remove("open");
+    });
+    detailbox.add("open");
+  }
 }
